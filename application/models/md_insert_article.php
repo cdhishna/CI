c59be1717fd461f74b49892a2ee1155179e09f2d
cdhishna <cdhishna@gmail.com>
2013-05-14 16:13:35 +0530

    Update md_insert_article.php
    removed article_id from the function, added comments!

--- application/models/md_insert_article.php
+++ application/models/md_insert_article.php
@@ -1,5 +1,5 @@
 <?php
-
+//the function takes in user article parameters and updates it to the 'article' table in the database, i.e. adds the article in database
 class Insert_new_article extends CI_Model {
 
         public function __construct()
@@ -12,12 +12,13 @@ class Insert_new_article extends CI_Model {
 	{
                 // Assuming that $params is the string containing user info separated by ",".
   		// Separate it from "," and add it to the article table.
-			// assuming that parameters are in the order: $articleid, $category_id,$sub_category_id,$userid,$article_header,$article_content
+			// assuming that parameters are in the order:  $category_id,$sub_category_id,$userid,$article_header,$article_content
 		$this->load->database();
 		$array = explode(",",$params);
+		//to find the date on ehich user posted the article
 		$date = date('d.m.y');
-        $query = $this->db->insert("INSERT INTO Article ( `article_id`,`article_cat_id`,`article_sub_cat_id`,`date_posted`,`posted_by`,`article_name`,
-`article_content`, `article_approval`) values ('".$array[0]."','".$array[1]."','".$array[2]."','".$date."','".$array[3]."','".$array[4]."','".$array[5]."','"0"')");
+        $query = $this->db->insert("INSERT INTO Article ( `article_cat_id`,`article_sub_cat_id`,`date_posted`,`posted_by`,`article_name`,
+`article_content`, `article_approval`) values ('".$array[0]."','".$array[1]."','".$date."','".$array[3]."','".$array[4]."','"0"')");
 
 		if($this->db->affected_rows() == 1))
 			return 1;
