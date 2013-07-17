fbc294420b8761484f79bdf66fe15014537477dc
cdhishna <cdhishna@gmail.com>
2013-07-17 23:07:51 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,6 @@
+   
+<ul class="tabs">  
+    <li><a href="javascript:tabSwitch('tab_1', 'content_1');" id="tab_1" class="active">Topics</a></li>  
+    <li><a href="javascript:tabSwitch('tab_2', 'content_2');" id="tab_2">Archives</a></li>  
+    <li><a href="javascript:tabSwitch('tab_3', 'content_3');" id="tab_3">Pages</a></li>  
+</ul> 
